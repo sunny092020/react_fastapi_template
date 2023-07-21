48d576b675b48dd609dce8642da336a00a0630dd
Vu Hoang Giang <itworkjp@gmail.com>
2023-07-21 10:15:59 +0700

    fix mysql

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -7,9 +7,9 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    first_name = Column(String)
-    last_name = Column(String)
-    hashed_password = Column(String, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
